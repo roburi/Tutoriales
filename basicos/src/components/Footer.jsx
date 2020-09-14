9b45633ff764c4ab6fcd89825b57975d9306d270
roburi <rvbr.rvbr@gmail.com>
2020-09-14 13:33:17 -0600

    Creando segundo componente

---
+++ basicos/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => (
+        
+        <footer>
+            <p>Todos los derechos reservados &copy;</p>
+        </footer>
+);
+
+
+ 
+export default Footer;
