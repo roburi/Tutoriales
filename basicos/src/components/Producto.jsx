332f1521ed7f1fcc760df570698c989b1a9c0826
roburi <rvbr.rvbr@gmail.com>
2020-09-14 16:45:03 -0600

    Eventos de react

--- basicos/src/components/Producto.jsx
+++ basicos/src/components/Producto.jsx
@@ -2,11 +2,20 @@ import React from 'react';
 
 function Producto({producto}) {
 
-    const {id, nombre, precio} = producto
+    const { nombre, precio} = producto
+    const seleccionarProdcuto = () =>{console.log('...comprando')}
+
     return(
         <div>
             <h2>{nombre}</h2>
             <p>${precio}</p>
+            <button 
+                type="button"
+                id="comprar"
+                onClick={ () => seleccionarProdcuto()}
+            >
+                Agregar
+            </button>
         </div>
     );
 }
