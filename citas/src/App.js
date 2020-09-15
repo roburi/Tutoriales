1af3000c2c8660de7f95320263d08fac1e581b31
roburi <rvbr.rvbr@gmail.com>
2020-09-15 12:11:47 -0600

    Mensaje condicional sin/con citas

--- citas/src/App.js
+++ citas/src/App.js
@@ -21,6 +21,9 @@ function App() {
 
   };
 
+  //Mensaje condicional
+  const titulo = citas.length === 0 ? 'No hay citas' : 'Administra tus citas'
+
   return (
     <Fragment>
       <h1>Administrador de Pacientes</h1>
@@ -32,7 +35,7 @@ function App() {
               crearCita={crearCita}/>
           </div>
           <div className="one-half column">
-            <h2>Administrar citas</h2>
+            <h2>{titulo}</h2>
             
             {citas.map(cita => (
               <Cita
