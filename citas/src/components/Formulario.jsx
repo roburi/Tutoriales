1a95acbf9496e6d6007ace2ae2676b58c0b9bb4b
roburi <rvbr.rvbr@gmail.com>
2020-09-15 10:13:12 -0600

    Leyendo lo que el usuario escribe en los inputs

--- citas/src/components/Formulario.jsx
+++ citas/src/components/Formulario.jsx
@@ -3,13 +3,22 @@ import React, {Fragment, useState} from 'react';
 function Formulario() {
 
     //State de citas
-    const [cita, actualizarcita] = useState({
+    const [cita, actualizarCita] = useState({
         mascota:'',
         propietario:'',
         fecha:'',
         hora:'',
         sintomas:''
     });
+
+    const actualizarState = e => {
+        actualizarCita({
+            ...cita,
+            [e.target.name] : e.target.value
+        })
+    };
+    //Extraer valores
+    const {mascota, propietario, fecha, hora, sintomas} = cita;
 
     return(
         <Fragment>
@@ -22,35 +31,40 @@ function Formulario() {
                     name="mascota" 
                     className="u-full-width" 
                     placeholder="Nombre mascota"
-                    onChange={actualizarcita}/>
+                    onChange={actualizarState}
+                    value={mascota}/>
 
                 <label>Nombre propietario</label>
                 <input 
                     type="text" 
-                    name="mascota" 
+                    name="propietario" 
                     className="u-full-width" 
                     placeholder="Nombre propietario" 
-                    onChange={actualizarcita}/>
+                    onChange={actualizarState}
+                    value={propietario}/>
             
                 <label>Fecha</label>
                 <input 
                     type="date" 
-                    name="mascota" 
+                    name="fecha" 
                     className="u-full-width" 
-                    onChange={actualizarcita}/>
+                    onChange={actualizarState}
+                    value={fecha}/>
 
                 <label>Hora</label>
                 <input 
                     type="time" 
-                    name="mascota" 
+                    name="hora" 
                     className="u-full-width" 
-                    onChange={actualizarcita}/>
+                    onChange={actualizarState}
+                    value={hora}/>
 
                 <label>Síntomas</label>
                 <textarea
                     className="u-full-width"
                     name="sintomas" 
-                    onChange={actualizarcita}></textarea>
+                    onChange={actualizarState}
+                    value={sintomas}></textarea>
                 
                 <button type="submit" className="u-full-width button-primary">Agregar Cita</button>
             </form>
