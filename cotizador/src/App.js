435191cf858b79862893145da6de6e32f1a13aa5
roburi <rvbr.rvbr@gmail.com>
2020-09-16 19:20:25 -0600

    creando el formulario

--- cotizador/src/App.js
+++ cotizador/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from './components/Header';
+import Formulario from './components/Formulario';
 
 import styled from '@emotion/styled';
 
@@ -22,7 +23,7 @@ function App() {
         titulo='Cotizador de Seguros'/>
   
       <ContenedorFormulario>
-
+        <Formulario/>
       </ContenedorFormulario>
   
     </Contenedor>
