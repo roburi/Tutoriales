5e1a04c9d92cdcde47dae950be59bf278959cc3f
roburi <rvbr.rvbr@gmail.com>
2020-09-24 12:08:35 -0600

    Validando formulario

--- imagenes/src/App.js
+++ imagenes/src/App.js
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Formulario from './components/Formulario';
 
 
 function App() {
+
+  const[busqueda, guardarBusqueda] = useState('');
+
   return (
     <div className="container center">
       <div className="jumbotron">
         <p className="lead text-center">Images and Videos, You'll find here!</p>
-
-        <Formulario />
+        <Formulario guardarBusqueda={guardarBusqueda}/>
       </div>
     </div>
   );
--- imagenes/src/components/Formulario.jsx
+++ imagenes/src/components/Formulario.jsx
@@ -1,15 +1,37 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Error from './Error'
 
 
-const Formulario = () => {
+const Formulario = ({guardarBusqueda}) => {
+
+    const[termino, guardarTermino] = useState('');
+    const[error, guardarError] = useState(false);
+
+    const buscarImagenes = e => {
+        e.preventDefault();
+
+        //Validar
+        if(termino.trim() === ''){
+            guardarError(true);
+            return;
+        }
+        guardarError(false);
+
+        //Enviar el termino de busqueda hacia el componente principal
+    }
+
     return ( 
-        <form>
+        <form onSubmit={buscarImagenes}>
             <div className="row">
+                <div className="form-group col-md-12">
+                    {error ? <Error mensaje="Error!! type something"/> : null}
+                </div>
                 <div className="form-group col-md-8">
                     <input
                         type="text"
                         className="form-control form-control-lg"
-                        placeholder="Find Here"/>
+                        placeholder="Find Here"
+                        onChange={e => guardarTermino(e.target.value)}/>
                 </div>
                 <div className="form-group col-md-2">
                     <input
