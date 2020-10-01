c13a31afbe2e7e60a073a801037fb0e9dbb96e24
roburi <rvbr.rvbr@gmail.com>
2020-09-30 18:29:06 -0600

    lo que falta

--- imagenes/src/components/Imagen.jsx
+++ imagenes/src/components/Imagen.jsx
@@ -8,16 +8,16 @@ const Imagen = ({imagen}) => {
         <div className="col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
             <div className="card">
                 <img src={previewURL} alt={tags} className="card-img-top"/>
-
                 <div className="card-body">
                     <p className="card-text">{likes} Likes</p>
                     <p className="card-text">{views} Views</p>
                 </div>
                 <div className="card-footer">
-                    <a href={largeImageURL} target="_blank" rel="noopener noreferrer"
-                        className="btn btn-primary btn-block">Go to</a>
+                    <a href={largeImageURL} 
+                       target="_blank" 
+                       rel="noopener noreferrer"
+                       className="btn btn-primary">Go to</a>
                 </div>
-
             </div>
         </div>
      );
