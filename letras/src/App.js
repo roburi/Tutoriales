b4c4252baea9719a8860b15d6cc7040ce17c58b7
roburi <rvbr.rvbr@gmail.com>
2020-10-05 13:39:13 -0600

    obteniendo la letra de una cancion

--- letras/src/App.js
+++ letras/src/App.js
@@ -1,11 +1,31 @@
-import React, {Fragment} from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 import Formulario from './components/Formulario';
+import axios from 'axios';
 
-
+ 
 function App() {
+ 
+  const [busquedaletra, guardarBusquedaLetra] = useState({});
+
+  useEffect(() => {
+    if(Object.keys(busquedaletra).length === 0) return;
+
+    const consultarAPIletra = async () => {
+      const {artista, cancion} = busquedaletra;
+
+      const url = `https://api.lyrics.ovh/v1/${artista}/${cancion}`;
+
+      const resultado = await axios(url);
+    }
+
+    consultarAPIletra();
+  },[busquedaletra]);
+
   return (
     <Fragment>
-      <Formulario />
+      <Formulario 
+        guardarBusquedaLetra={guardarBusquedaLetra}
+      />
     </Fragment>
   );
 }
--- letras/src/components/Formulario.jsx
+++ letras/src/components/Formulario.jsx
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 
-const Formulario = () => {
+const Formulario = ({guardarBusquedaLetra}) => {
 
     const[busqueda, guardarBusqueda] = useState({
         artista:'',
@@ -9,14 +9,14 @@ const Formulario = () => {
 
     const[error, guardarError] = useState(false);
 
-    const {artista, cancion} = busqueda;
+    const { artista, cancion } = busqueda;
 
     //Funcion a cada input
     const actualizarState = e => {
         guardarBusqueda({
             ...busqueda,
             [e.target.name] : e.target.value
-        });
+        })
     };
 
     const buscarInformacion = e =>{
@@ -24,19 +24,28 @@ const Formulario = () => {
 
         if(artista.trim() === '' || cancion.trim() === ''){
             guardarError(true);
+            return;
         }
         guardarError(false);
 
-        
+        guardarBusquedaLetra(busqueda);
     };
 
     return ( 
         <div className="bg-info">
+
+            {error ? 
+                <p className="alert alert-danger text-center p-2">
+                    Todos los campos son obligatorios
+                </p> 
+            : null}
+            
             <div className="container">
                 <div className="row">
                     <form
                         onSubmit={buscarInformacion} 
-                        className="col card text-white bg-transparent mb-5 pt-5 pb-2">
+                        className="col card text-white bg-transparent mb-5 pt-5 pb-2"
+                    >
                         <fieldset>
                             <legend className="text-center">Buscador Letras</legend>
                             <div className="row">
@@ -48,7 +57,7 @@ const Formulario = () => {
                                             className="form-control"
                                             name="artista"
                                             placeholder="Nombre.."
-                                            onChange={guardarBusqueda}
+                                            onChange={actualizarState}
                                             value={artista}/>
                                     </div>
                                 </div>
@@ -60,7 +69,7 @@ const Formulario = () => {
                                             className="form-control"
                                             name="cancion"
                                             placeholder="Nombre.."
-                                            onChange={guardarBusqueda}
+                                            onChange={actualizarState}
                                             value={cancion}/>
                                     </div>
                                 </div>
