0ba64080eb53aad3f8cb5898f03ca43e0b6d6707
roburi <rvbr.rvbr@gmail.com>
2020-10-15 17:45:34 -0600

    cambios

---
+++ MERNTasks/cliente/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './index.css';
+
+import Login from './components/auth/Login';
+import NuevaCuenta from './components/auth/NuevaCuenta';
+import Proyectos from './components/proyectos/Proyectos';
+
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Login} />
+        <Route exact path="/nueva-cuenta" component={NuevaCuenta} />
+        <Route exact path="/proyectos" component={Proyectos} />
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
