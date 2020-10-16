9e5bf76a35d5d65b6ee625faac2be0331667c299
roburi <rvbr.rvbr@gmail.com>
2020-10-16 12:17:24 -0600

    Terminado Login y nueva cuenta

--- MERNTasks/cliente/src/components/auth/Login.jsx
+++ MERNTasks/cliente/src/components/auth/Login.jsx
@@ -1,9 +1,65 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {Link} from 'react-router-dom';
 
 const Login = () => {
+
+    //para inicio de sesion
+    const[usuario, guardarUsuario] = useState({
+        email: '',
+        password: ''
+    });
+
+    const { email, password } = usuario;
+
+    const onChange = e => {
+        guardarUsuario({
+            ...usuario,
+            [e.target.name] : e.target.value
+        })
+    }
+
+    const onSubmit = e =>{
+        e.preventDefault();
+
+        //validar datos vacios
+
+
+    }
+
     return ( 
-        <h1>Login</h1>
+        <div className="form-usuario">
+            <div className="contenedor-form sombra-dark">
+                <h1>Iniciar Sesion</h1>
+                <form onSubmit={onSubmit}>
+
+                    <div className="campo-form">
+                        <label htmlFor="email">Email</label>
+                        <input
+                            type="email"
+                            id="email"
+                            name="email"
+                            placeholder="Tu email"
+                            value={email}
+                            onChange={onChange}/>
+                    </div>
+                    <div className="campo-form">
+                        <label htmlFor="password">Password</label>
+                        <input
+                            type="password"
+                            id="password"
+                            name="password"
+                            value={password}
+                            placeholder="Tu password"
+                            onChange={onChange}/>
+                    </div>
+                    <div className="campo-form">
+                        <input type="submit" className="btn btn-primario btn-block" value="Iniciar Sesión" />
+                    </div>
+                    <Link to={'/nueva-cuenta'} className="enlace-cuenta">Crear cuenta</Link>
+                </form>
+            </div>
+        </div>
      );
 }
- 
+
 export default Login;
--- MERNTasks/cliente/src/components/auth/NuevaCuenta.jsx
+++ MERNTasks/cliente/src/components/auth/NuevaCuenta.jsx
@@ -1,9 +1,86 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {Link} from 'react-router-dom';
 
 const NuevaCuenta = () => {
+
+    //para inicio de sesion
+    const[usuario, guardarUsuario] = useState({
+        email: '',
+        password: '',
+        nombre: '',
+        confirmar: ''
+    });
+
+    const { email, password, nombre, confirmar } = usuario;
+
+    const onChange = e => {
+        guardarUsuario({
+            ...usuario,
+            [e.target.name] : e.target.value
+        })
+    }
+
+    const onSubmit = e =>{
+        e.preventDefault();
+
+        //validar datos vacios
+
+    }
+
     return ( 
-        <h1>NuevaCuenta</h1>
+        <div className="form-usuario">
+            <div className="contenedor-form sombra-dark">
+                <h1>Nueva Cuenta</h1>
+                <form onSubmit={onSubmit}>
+
+                    <div className="campo-form">
+                        <label htmlFor="nombre">Nombre</label>
+                        <input
+                            type="text"
+                            id="nombre"
+                            name="nombre"
+                            placeholder="Tu Nombre"
+                            value={nombre}
+                            onChange={onChange}/>
+                    </div>
+                    <div className="campo-form">
+                        <label htmlFor="email">Email</label>
+                        <input
+                            type="email"
+                            id="email"
+                            name="email"
+                            placeholder="Tu email"
+                            value={email}
+                            onChange={onChange}/>
+                    </div>
+                    <div className="campo-form">
+                        <label htmlFor="password">Password</label>
+                        <input
+                            type="password"
+                            id="password"
+                            name="password"
+                            value={password}
+                            placeholder="Tu password"
+                            onChange={onChange}/>
+                    </div>
+                    <div className="campo-form">
+                        <label htmlFor="confirmar">Confirmar Password</label>
+                        <input
+                            type="password"
+                            id="confirmar"
+                            name="confirmar"
+                            value={confirmar}
+                            placeholder="Confirma tu password"
+                            onChange={onChange}/>
+                    </div>
+                    <div className="campo-form">
+                        <input type="submit" className="btn btn-primario btn-block" value="Registrar" />
+                    </div>
+                    <Link to={'/'} className="enlace-cuenta">Volver a Iniciar Sesion</Link>
+                </form>
+            </div>
+        </div>
      );
 }
- 
+
 export default NuevaCuenta;
